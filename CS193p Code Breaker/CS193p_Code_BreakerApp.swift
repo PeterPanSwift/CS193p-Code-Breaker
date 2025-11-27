ae3c5aee39fed8132b502f731a9ebc594b27c8cc
Peter Pan <apppeterpan@gmail.com>
2025-11-27 18:39:57 +0800

    Initial Commit

---
+++ CS193p Code Breaker/CS193p_Code_BreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193p_Code_BreakerApp.swift
+//  CS193p Code Breaker
+//
+//  Created by SHIH-YING PAN on 2025/11/27.
+//
+
+import SwiftUI
+
+@main
+struct CS193p_Code_BreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CS193p Code Breaker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CS193p Code Breaker
+//
+//  Created by SHIH-YING PAN on 2025/11/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
